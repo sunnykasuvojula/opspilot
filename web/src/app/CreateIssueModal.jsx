7350f6bc1526b0383ae8c123614e0f7702c22a76
sunny kasuvojula <144178636+sunnykasuvojula@users.noreply.github.com>
2026-02-08 00:12:35 -0500

    implement create issue functionality

--- web/src/app/CreateIssueModal.jsx
+++ web/src/app/CreateIssueModal.jsx
@@ -1,20 +1,47 @@
+import api from "@/lib/api";
 import { useState } from "react";
 const CreateIssueModal=({workspaceId, projectId, onClose, onIssueCreated})=>{
-    const [title, setTitle]=useState("");
     const[error,setError]=useState("");
-    const[description,setDescription]=useState("");
-    const[bug,setbugtype]=useState("");
-    const[status,setstatusType]=useState("");
-    const[priority,setPiority]=useState("");
-    const [asignee, setAsignee]=useState("");
     const[loading,setLoading]=useState(false);
+    const [form,setForm]=useState({
+        title:"",
+        description:"",
+        bug:"",
+        status:"",
+        priority:"",
+        asignee:""  
+    });
+
+    const handleChange=(e)=>{
+        setForm((prev)=>({
+            ...prev,
+            [e.target.name]:e.target.value
+        }))
+    }
 
     const handleSubmit=async(e)=>{
         e.preventDefault();
-        ["title","description","bug","status","priority","asignee"].map((field)=>{
-            if(!field)
-            return setError(`${field} is required`);
-        })
+        for (const key in form) {
+            if (!form[key]) {
+              setError(`${key} is required`);
+              return;
+            }
+          }
+        setError("");
+        setLoading(true);
+        try{
+            const {data}=await api.post(`/api/auth/workspaces/${workspaceId}/projects/${projectId}/issues`,form);
+            onIssueCreated(data.issue);
+            console.log(data.issue);
+            onClose();
+        }
+        catch(error)
+        {
+            setError(error.response?.data?.message || "Failed to create issue");
+        }
+        finally{
+            setLoading(false);
+        }
     }
     return(
         <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center z-50">
@@ -24,48 +51,54 @@ const CreateIssueModal=({workspaceId, projectId, onClose, onIssueCreated})=>{
                 <form onSubmit={handleSubmit} className="space-y-3">
                     <input
                         type="text"
+                        name="title"
                         placeholder="Issue Title"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
+                        value={form.title}
+                        onChange={handleChange}
                         className="w-full p-2 border rounded"
                         required
                     />
                     <input
                         type="text"
+                        name="description"
                         placeholder="Issue Description"
                         className="w-full p-2 border rounded"
-                        value={description}
-                        onChange={(e)=> setDescription(e.target.value)}
+                        value={form.description}
+                        onChange={handleChange}
                         required
                     />
                     <input
                         type="text"
+                        name="bug"
                         placeholder="Bug Type"
                         className="w-full p-2 border rounded"
-                        value={bug}
-                        onChange={(e)=> setbugtype(e.target.value)}
+                        value={form.bug}
+                        onChange={handleChange}
                         required
                     />
                     <input
                         type="text"
+                        name="status"
                         placeholder="Status"
                         className="w-full p-2 border rounded"
-                        value={status}
-                        onChange={(e)=> setstatusType(e.target.value)}
+                        value={form.status}
+                        onChange={handleChange}
                         required
                     />
                     <input type="text" 
+                        name="priority"
                         placeholder="priority"
                         className="w-full p-2 border rounded"   
-                        value={priority}
-                        onChange={(e)=> setPiority(e.target.value)}
+                        value={form.priority}
+                        onChange={handleChange}
                         required
                     />
                     <input type="text" 
+                        name="asignee"
                         placeholder="asignee"
                         className="w-full p-2 border rounded"   
-                        value={priority}
-                        onChange={(e)=> setAsignee(e.target.value)}
+                        value={form.asignee}
+                        onChange={handleChange}
                         required
                     />                   
                     <div className="flex justify-end gap-2 mt-4">
